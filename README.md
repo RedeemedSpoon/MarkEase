0c7461f6343b13ff1dea9059202055ebefa68093
Ethan <QustoTemmo@protonmail.com>
2023-12-25 00:09:46 +0100

    MarkEase V1

---
+++ README.md
@@ -0,0 +1,94 @@
+# MarkEase - Minimal Yet versatile Image Watermarking Tool
+
+![MarkEase Screenshot](./Images/Logo.png)
+
+## Introduction
+
+MarkEase is a sophisticated yet user-friendly Python application tailored for watermarking images. Designed with simplicity and efficiency in mind, this tool offers a comprehensive suite of features to enhance your images with text, shapes, and other customizable elements. Whether you're a professional photographer, graphic designer, or just someone looking to personalize your images, MarkEase is the go-to solution that simplifies the watermarking process. Very simple Solution to replace Photoshop or Gimp. No learning curve, MarkEase has everything you need to get started.
+
+## Features
+
+### 1. Import Images
+
+-  **Versatile Format Support**: MarkEase allows users to effortlessly import images in various formats, including JPEG, PNG, and GIF, ensuring compatibility with most image files you work with.
+-  **User-Friendly Interface**: The import feature is seamlessly integrated into the application's intuitive user interface, enabling quick and hassle-free image uploads.
+
+### 2. Text Watermarking
+
+-  **Customizable Text Elements**: MarkEase empowers users to add custom text to their images, providing options to select from a range of font sizes, styles, and colors.
+-  **Real-Time Preview**: Users can preview the text watermark in real-time, allowing for instant adjustments and ensuring optimal placement and appearance.
+
+### 3. Shape Watermarking
+
+-  **Diverse Shape Options**: With MarkEase, users can insert a variety of shapes such as rectangles and circles onto their images, enhancing creativity and versatility.
+-  **Size and Positioning Controls**: The application offers precise size and positioning controls for shape elements, enabling users to achieve the desired look and feel.
+
+### 4. Image Watermarking
+
+-  **Overlay Images Seamlessly**: MarkEase facilitates the overlaying of images as watermarks, complete with adjustable sizes, opacity levels, and blending options.
+-  **High-Quality Output**: The application ensures high-quality output, maintaining image integrity while adding watermarks, making it ideal for professional use.
+
+### 5. Adjust Opacity
+
+-  **Transparency Controls**: MarkEase provides users with the ability to adjust the opacity of added elements, offering seamless integration with existing images and backgrounds.
+-  **Visual Feedback**: The application offers visual feedback on opacity adjustments, allowing users to fine-tune settings for optimal results.
+
+### 6. Color Settings
+
+-  **Custom Color Selection**: MarkEase offers extensive color customization options, enabling users to select specific colors for text or shape elements based on their preferences.
+-  **Color Picker Tool**: The integrated color picker tool simplifies the color selection process, offering a wide spectrum of color choices and shades.
+
+## How to Use
+
+1. **Installation**: To get started, clone the repository using the following command:
+
+   ```bash
+   git clone https://github.com/RedeemedSpoon/MarkEase.git
+   ```
+
+   Once cloned, navigate to the project directory and install the required packages using pip:
+
+   ```bash
+   cd MarkEase
+   pip install -r requirements.txt
+   ```
+
+2. **Launch Application**: After installing the necessary packages, execute the `MarkEase.py` script to launch the application and access its intuitive interface:
+   ```bash
+   python MarkEase.py
+   ```
+   This command will initiate the MarkEase application, allowing you to import, edit, and export images seamlessly.
+3. **Import Image**: Utilize the "Import An Image" button to browse, select, and import an image file for watermarking.
+4. **Apply Watermark**: Utilize the diverse set of tools available within MarkEase to apply text, shapes, and image watermarks to your imported image.
+5. **Save/Export**: Upon completion, save or export your edited image using the "Export" feature, ensuring you retain the watermark and desired modifications.
+
+## Limitations
+
+### 1. Supported Formats
+
+-  **Limited Format Support**: While MarkEase supports popular formats like JPEG, PNG, and GIF, it may not accommodate less common or proprietary image formats.
+
+### 2. Text Limitations
+
+-  **Basic Text Features**: The text watermarking feature within MarkEase offers basic text formatting options, potentially limiting advanced customization requirements.
+
+### 3. Shape Customization
+
+-  **Basic Shape Options**: MarkEase primarily focuses on offering basic shape options such as rectangles and circles, potentially restricting users requiring more complex or intricate shapes.
+
+### 4. Performance
+
+-  **Processing Constraints**: Processing large images or applying multiple watermarks simultaneously within MarkEase may result in performance constraints or increased processing times.
+
+## Screenshots
+
+![Screenshot 1](./Images/home.png)
+![Screenshot 2](./Images/edit.png)
+
+## Contributing
+
+We welcome contributions from the community! Please refer to the [CONTRIBUTING.md](CONTRIBUTING.md) file for comprehensive guidelines on how to contribute to the MarkEase project.
+
+## License
+
+MarkEase is licensed under the MIT License. Refer to the [LICENSE](LICENSE) file for detailed license information and terms.
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 from tkinter import ttk, filedialog, messagebox
-from PIL import Image, ImageTk, UnidentifiedImageError
+from PIL import Image, ImageTk, UnidentifiedImageError, ImageDraw, ImageFont
 import webbrowser, os, tkcolorpicker
 
 class MarkEase:
@@ -75,8 +75,8 @@ class MarkEase:
         self.canvas.tag_bind(self.credit, "<Leave>", lambda event: self.canvas.config(cursor=""))
         
         self.options = {"JPEG Files": "*.jpg","PNG Files": "*.png","GIF Files": "*.gif","All Files": "*.*"}
-        self.font_sizes = ["6", "8", "10", "12", "14", "16", "18"]
-        self.watermark_width = ["X-Small", "Small", "Medium", "Big"]
+        self.font_sizes = ["8", "10", "12", "14", "16", "18", "20", "22", "24", "26"]
+        self.thing_width = ["X-Small", "Small", "Medium", "Big"]
         self.items_to_delete = [item for item in self.canvas.find_all() if all(tag not in self.canvas.gettags(item) for tag in ["background", "brand", "link", "side"])]
 
     def startup(self):
@@ -92,8 +92,8 @@ class MarkEase:
             button.destroy()
             
         self.buttons_list.clear()
-        self.opacity = 100
-        self.color = "#000000"
+        self.opacity = 255
+        self.color = (0, 0, 0)
         self.is_setup_screen = True
         
         self.slogan = self.canvas.create_text(640, 300, anchor="center")
@@ -113,7 +113,7 @@ class MarkEase:
         self.image_on_canvas = self.canvas.create_image((700, 337.5), anchor=tk.CENTER, image=self.photo)
         self.file_name = os.path.basename(self.file_path)
         self.root.title(f"MarkEase - {self.file_name}")
-        self.name = self.canvas.create_text(700, 75, text=self.file_name, fill="white", font=("Noto Sans", 20, "bold"))
+        self.name = self.canvas.create_text(700, 75, text=self.file_name, fill="white", font=("Noto Sans", 20, "bold")) 
         
         self.home_arrow_id = self.canvas.create_image(1255, 25, anchor=tk.E, image=self.arrow_icon, tags='side')
         self.home_text_id = self.canvas.create_text(1180, 22.5, text="Home", anchor=tk.W, font=("Noto Sans", 12), fill="white", tags='side')
@@ -122,7 +122,7 @@ class MarkEase:
         self.canvas.tag_bind(self.home_text_id, "<Leave>", lambda event: self.canvas.config(cursor=""))
         self.canvas.tag_bind(self.home_arrow_id, "<Enter>", lambda event: self.canvas.config(cursor="hand2"))
         self.canvas.tag_bind(self.home_arrow_id, "<Leave>", lambda event: self.canvas.config(cursor=""))
-        
+            
         button_data = [
             {"text": "Export", "compound": tk.LEFT, "image": self.download_icon, "command": self.export_image, "style": "TButton"},
             {"text": "Choose Another", "compound": tk.LEFT, "image": self.change_icon, "command": self.import_image, "style": "A.TButton"},
@@ -154,45 +154,84 @@ class MarkEase:
     def set_cursor(self, cursor_type):
         self.current_cursor = cursor_type
         self.canvas.config(cursor=cursor_type)
-            
+        
+    def update_image(self):
+        self.photo = ImageTk.PhotoImage(self.image)
+        self.canvas.itemconfig(self.image_on_canvas, image=self.photo)
+
     def open_link(self, event):
-        webbrowser.open("https://www.youtube.com/watch?v=XlYsRxgWxNo")
+        webbrowser.open("https://github.com/RedeemedSpoon/MarkEase")
 
     def import_image(self):
         self.file_path = filedialog.askopenfilename(filetypes=list(self.options.items()), initialdir='~/', title="Select an image")
         if self.file_path:
             try: 
                 self.image = Image.open(self.file_path)
-                aspect_ratio = 720 / 405
-                width, height = self.image.size
-                image_ratio = width / height
-                if image_ratio > aspect_ratio:
-                    new_width = 720
-                    new_height = int(720 / 405)
+                self.width, self.height = self.image.size
+                image_ratio = self.width / self.height
+                
+                if image_ratio > 1.7777:
+                    self.new_width = 720
+                    self.new_height = 405
                 else:
-                    new_height = 405
-                    new_width = int(405 * image_ratio)
+                    self.new_height = 405
+                    self.new_width = int(405 * image_ratio)
                     
-                self.image = self.image.resize((new_width, new_height), Image.ADAPTIVE)
+                self.width_diff = self.width / self.new_width
+                self.height_diff =  self.height / self.new_height
+                self.og = self.image.copy()
+                self.og_copy = self.og.copy()
+                
+                self.image = self.image.resize((self.new_width, self.new_height), Image.ADAPTIVE)
                 self.photo = ImageTk.PhotoImage(self.image)
-                self.copy = self.photo
+                self.copy = self.image.copy()
                 self.canvas.itemconfig(self.name, text="")
                 self.start_edit()
+
             except UnidentifiedImageError:
                 messagebox.showerror(title='Error!', message="Error, Not a valid image!")
 
     def export_image(self):
         save_path = filedialog.asksaveasfilename(defaultextension=".png", filetypes=list(self.options.items()))
         if save_path:
-            self.image.save(save_path)
+            self.og.save(save_path)
             messagebox.showinfo("Success", "Image saved successfully!")
+            
+    def draw(self, x, y):
+        self.drawing = ImageDraw.Draw(self.image, 'RGBA')
+        self.mouse_x = int(x - ((1280 - self.new_width) / 2) - 60)
+        self.mouse_y = int(y - ((720 - self.new_height) / 2) + 5)
+        self.rgba = self.color + (self.opacity,)
+        
+        self.real = ImageDraw.Draw(self.og, 'RGBA')
+        self.real_mouse_x = int((x - ((1280 - self.new_width) / 2) - 60) * self.width_diff)
+        self.real_mouse_y = int((y - ((720 - self.new_height) / 2) + 5) * self.height_diff)
+
+    def thing_size(self, input):
+        if input == 'X-Small':
+            self.size = 25
+        if input == 'Small':
+            self.size = 50
+        elif input == 'Medium':
+            self.size = 100
+        elif input == 'Big':
+            self.size = 250
+        
+    def draw_text(self, event):
+        self.draw(x=event.x, y=event.y)
+        self.font = ImageFont.load_default(int(self.font_size))
+        self.real_font = ImageFont.load_default(int(float(self.font_size) * self.width_diff))
+        self.drawing.text((self.mouse_x, self.mouse_y), self.user_text, fill=self.rgba, font=self.font)
+        self.real.text((self.real_mouse_x, self.real_mouse_y), self.user_text, fill=self.rgba, font=self.real_font)
+        self.update_image()   
+        self.canvas.unbind("<Button-1>")
 
     def text(self, finish=False):
         if finish:
             self.user_text = self.text_entry.get() 
             self.font_size = self.font_size_combobox.get()
-            self.text_window.destroy()
-            # Your Code Goes Here
+            self.text_window.destroy()           
+            self.canvas.bind("<Button-1>", self.draw_text)
         else:
             self.text_window = tk.Toplevel()
             self.text_window.title("Text Settings")
@@ -210,37 +249,77 @@ class MarkEase:
             
             ttk.Button(self.text_window, text="OK", command=lambda: self.text(True)).grid(row=2, column=0, padx=30, pady=30)
             ttk.Button(self.text_window, text="Cancel", command=self.text_window.destroy).grid(row=2, column=1, padx=30, pady=30)
+            
+    def draw_shape(self, event):
+        self.draw(x=event.x, y=event.y)
+        x1, y1 = self.mouse_x, self.mouse_y
+        x2, y2 = x1 + self.size, y1 + self.size
+        real_x1, real_y1 = self.real_mouse_x, self.real_mouse_y
+        real_x2, real_y2 = real_x1 + (self.size * self.width_diff), real_y1 + (self.size * self.height_diff)
+        shape_args = [(x1, y1, x2, y2), self.rgba]
+        real_shape_args = [(real_x1, real_y1, real_x2, real_y2), self.rgba]
         
+        if self.user_shape == "Rectangle":
+            self.drawing.rectangle(*shape_args)
+            self.real.rectangle(*real_shape_args)
+        elif self.user_shape == "Circle":
+            self.drawing.ellipse(*shape_args)
+            self.real.ellipse(*real_shape_args)
+
+        self.update_image()
+        self.canvas.unbind("<Button-1>")
+            
     def shape(self, finish=False):
         if finish:
             self.user_shape = self.shape_var.get()
-            self.user_fill = self.fill_var.get()
-            self.text_window.destroy()
-            #Your Code Goes Here
+            self.thing_size(self.shape_resolution.get())
+            self.shape_window.destroy()
+            self.canvas.bind("<Button-1>", self.draw_shape)
         else:
-            self.text_window = tk.Toplevel()
-            self.text_window.title("Shape & Fill Settings")
-            self.text_window.geometry("500x225")
-            self.text_window.resizable(False, False)
+            self.shape_window = tk.Toplevel()
+            self.shape_window.title("Shape & Size Settings")
+            self.shape_window.geometry("500x250")
+            self.shape_window.resizable(False, False)
 
-            ttk.Label(self.text_window, text="Select Shape:").grid(row=0, column=0, padx=10, pady=20)
+            ttk.Label(self.shape_window, text="Select Shape:").grid(row=0, column=0, padx=10, pady=20)
             self.shape_var = tk.StringVar(value="Rectangle")
-            ttk.Radiobutton(self.text_window, text="Rectangle", variable=self.shape_var, value="Rectangle").grid(row=0, column=1, padx=10, pady=10)
-            ttk.Radiobutton(self.text_window, text="Circle", variable=self.shape_var, value="Circle").grid(row=0, column=2, padx=10, pady=10)
+            ttk.Radiobutton(self.shape_window, text="Rectangle", variable=self.shape_var, value="Rectangle").grid(row=0, column=1, padx=10, pady=10)
+            ttk.Radiobutton(self.shape_window, text="Circle", variable=self.shape_var, value="Circle").grid(row=0, column=2, padx=10, pady=10)
 
-            ttk.Label(self.text_window, text="Select Fill:").grid(row=1, column=0, padx=10, pady=10)
-            self.fill_var = tk.StringVar(value="Fill")
-            ttk.Radiobutton(self.text_window, text="Fill", variable=self.fill_var, value="Fill").grid(row=1, column=1, padx=10, pady=10)
-            ttk.Radiobutton(self.text_window, text="No Fill", variable=self.fill_var, value="No Fill").grid(row=1, column=2, padx=10, pady=10)
+            ttk.Label(self.shape_window, text="How big is the shape?").grid(row=1, column=0, padx=10, pady=10)
+            self.shape_resolution = ttk.Combobox(self.shape_window, values=self.thing_width, width=10)
+            self.shape_resolution.set("Small")
+            self.shape_resolution.grid(row=1, column=1, columnspan=2, padx=10, pady=10)
 
-            ttk.Button(self.text_window, text="OK", command=lambda: self.shape(True)).grid(row=2, column=2, padx=30, pady=30)
-            ttk.Button(self.text_window, text="Cancel", command=self.text_window.destroy).grid(row=2, column=1, padx=30, pady=30)
+            ttk.Button(self.shape_window, text="OK", command=lambda: self.shape(True)).grid(row=3, column=0, padx=30, pady=30)
+            ttk.Button(self.shape_window, text="Cancel", command=self.shape_window.destroy).grid(row=3, column=2, padx=30, pady=30)
         
+    def print_watermark(self, event):
+        self.draw(x=event.x, y=event.y)
+        
+        original_width, original_height = self.watermark_img.size
+        if original_width > original_height:
+            new_width = self.size
+            new_height = int(original_height * (self.size / original_width))
+        else:
+            new_width = int(original_width * (self.size / original_height))
+            new_height = self.size
+        
+        watermark_resized = self.watermark_img.resize((new_width, new_height))
+        real_watermark_resized = self.watermark_img.resize((int(new_width * self.width_diff), int(new_height * self.height_diff)))
+        mask = watermark_resized.convert("L").point(lambda x: int(x * (float(self.opacity) / 255)))
+        real_mask =real_watermark_resized.convert("L").point(lambda x: int(x * (float(self.opacity) / 255)))
+        self.image.paste(watermark_resized, (self.mouse_x, self.mouse_y), mask=mask)
+        self.og.paste(real_watermark_resized, (self.real_mouse_x, self.real_mouse_y), mask=real_mask)
+        self.update_image()
+        self.canvas.unbind("<Button-1>")
+
     def watermark(self, finish=False):
         if finish:
-            self.watermark_resoltion = self.image_resolution.get()
+            self.thing_size(self.image_resolution.get())
             self.watermark_window.destroy()
-            #Your Code Goes Here
+            self.canvas.bind("<Button-1>", self.print_watermark)
+            
         else:
             self.file_path = filedialog.askopenfilename(filetypes=list(self.options.items()), initialdir='~/', title="Select an image")
             if self.file_path:
@@ -255,7 +334,7 @@ class MarkEase:
                     self.watermark_window.resizable(False, False)
 
                     ttk.Label(self.watermark_window, text="How big is the watermark?", font=("Noto Sans", 15)).pack(pady=10)
-                    self.image_resolution = ttk.Combobox(self.watermark_window, values=self.watermark_width, width=10)
+                    self.image_resolution = ttk.Combobox(self.watermark_window, values=self.thing_width, width=10)
                     self.image_resolution.set("Small")
                     self.image_resolution.pack(pady=10)
                     
@@ -276,7 +355,7 @@ class MarkEase:
             label = tk.Label(self.opacity_window, text="Please select the level of opacity you want", font=("Noto Sans", 12))
             label.pack(pady=10)
             
-            self.slider = tk.Scale(self.opacity_window, from_=0, to=100, orient="horizontal", length=200)
+            self.slider = tk.Scale(self.opacity_window, from_=0, to=255, orient="horizontal", length=200)
             self.slider.set(self.opacity)
             self.slider.pack(padx=20)
 
@@ -288,11 +367,14 @@ class MarkEase:
     def colorize(self):
         selected_color = tkcolorpicker.askcolor(title="Color Settings", color=self.color)[1]
         self.color = selected_color if selected_color else self.color
+        self.color = tuple(int(self.color.lstrip('#')[i:i+2], 16) for i in (0, 2, 4))
 
     def undo(self):
         self.image = self.copy
-        self.opacity = 100
-        self.color = '#000000'
+        self.og = self.og_copy
+        self.update_image()
+        self.opacity = 255
+        self.color = (0, 0, 0)
 
 if __name__ == '__main__':
     root = MarkEase()
